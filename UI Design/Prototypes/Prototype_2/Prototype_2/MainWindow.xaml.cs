82f402fdf34b4a584ab91bc82ec3565ba202709f
raytheon <daprank007@gmail.com>
2011-08-06 11:22:31 +0200

    add tray icon notification

--- UI Design/Prototypes/Prototype_2/Prototype_2/MainWindow.xaml.cs
+++ UI Design/Prototypes/Prototype_2/Prototype_2/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Shapes;
 using System.Windows.Interop;
 using System.ComponentModel;
 using System.Net;
+using System.Diagnostics;
 
 namespace Prototype_2
 {
@@ -21,11 +22,25 @@ namespace Prototype_2
     {
         readonly IsYouTubeVideoSpecification _youTubeUrlIdentifier;
 
+        readonly System.Windows.Forms.NotifyIcon _notifier;
+        WindowState _storedWindowState;
+
+        string _currentDownloadFilename;
+
+        const string ICON_LOCATION = @"Icons\trayicon.ico";
+
         public MainWindow()
         {
             InitializeComponent();
 
             _youTubeUrlIdentifier = new IsYouTubeVideoSpecification();
+
+            _storedWindowState = WindowState.Normal;
+
+            _notifier = new System.Windows.Forms.NotifyIcon();
+
+
+            InitializeTrayIcon();
         }
 
         protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
@@ -66,6 +81,8 @@ namespace Prototype_2
         void DownloadFileCompletedEventHandler(object sender, AsyncCompletedEventArgs e)
         {
             Status.Text = string.Format("Done");
+
+            ShowNewDownloadNotification(string.Format("{0} download completed. Click to open containing folder.", _currentDownloadFilename));
         }
 
         void DownloadProgressChangedEventHandler(object sender, DownloadProgressChangedEventArgs e)
@@ -91,11 +108,32 @@ namespace Prototype_2
             youTubeDownloader.DownloadFileCompleted += DownloadFileCompletedEventHandler;
             youTubeDownloader.DownloadProgressChanged += DownloadProgressChangedEventHandler;
 
-            string filepath = System.IO.Path.ChangeExtension(System.IO.Path.Combine(Properties.Settings.Default.DownloadPath, youTubeDownloader.GetTitle()), "flv");
+            string videoTitle = youTubeDownloader.GetTitle();
+
+            _currentDownloadFilename = videoTitle;
+
+            string filepath = System.IO.Path.ChangeExtension(System.IO.Path.Combine(Properties.Settings.Default.DownloadPath, videoTitle), "flv");
 
             youTubeDownloader.DownloadAsync(filepath);
 
-            Filename.Text = youTubeDownloader.GetTitle();
+            Filename.Text = videoTitle;
+
+            ShowNewDownloadNotification(string.Format("Downloading {0}", videoTitle));
+        }
+
+        void InitializeTrayIcon()
+        {
+            _notifier.BalloonTipTitle = this.Title;
+            _notifier.Text = this.Title;
+            _notifier.Icon = new System.Drawing.Icon(ICON_LOCATION);
+            _notifier.Click += new EventHandler(m_notifyIcon_Click);
+
+            _notifier.BalloonTipClicked += new EventHandler(_notifier_BalloonTipClicked);
+        }
+
+        void _notifier_BalloonTipClicked(object sender, EventArgs e)
+        {
+            Process.Start(Properties.Settings.Default.DownloadPath);
         }
 
         void RegisterClipboardListener()
@@ -125,6 +163,51 @@ namespace Prototype_2
             var settings = new SettingsWindow();
 
             settings.ShowDialog();
+        }
+
+        void OnClose(object sender, CancelEventArgs args)
+        {
+            _notifier.Dispose();
+        }
+
+        void OnStateChanged(object sender, EventArgs args)
+        {
+            if (WindowState == WindowState.Minimized)
+            {
+                Visibility = System.Windows.Visibility.Collapsed;
+            }
+            else
+            {
+                _storedWindowState = WindowState;
+            }
+        }
+        void OnIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs args)
+        {
+            CheckTrayIcon();
+        }
+
+        void m_notifyIcon_Click(object sender, EventArgs e)
+        {
+            Visibility = System.Windows.Visibility.Visible;
+            WindowState = _storedWindowState;
+        }
+        void CheckTrayIcon()
+        {
+            ShowTrayIcon(!IsVisible);
+        }
+
+        void ShowTrayIcon(bool show)
+        {
+            if (_notifier != null)
+            {
+                _notifier.Visible = show;
+            }
+        }
+
+        void ShowNewDownloadNotification(string message)
+        {
+            _notifier.BalloonTipText = message;
+            _notifier.ShowBalloonTip(1500);
         }
     }
 }
